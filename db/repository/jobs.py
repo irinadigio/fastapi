8de2efe922346fec2afab9a3f7d6160f2dd5362b
irudigio <irina.digiovanni@intive.com>
2022-01-10 16:51:57 -0300

    add files

--- db/repository/jobs.py
+++ db/repository/jobs.py
@@ -40,3 +40,6 @@ def delete_job_by_id(id:int,db:Session,owner_id):
     db.commit()
     return 1
 
+def search_job(query: str, db: Session):
+    jobs = db.query(Job).filter(Job.title.contains(query))
+    return jobs
--- webapps/jobs/route_jobs.py
+++ webapps/jobs/route_jobs.py
@@ -8,13 +8,14 @@ from db.repository.jobs import list_jobs
 from sqlalchemy.orm import Session
 from db.session import get_db
 from fastapi import Depends
-from db.repository.jobs import retreive_job, search_job
+from db.repository.jobs import retreive_job, search_job 
 
 from db.models.users import User  
 from apis.version1.route_login import get_current_user_from_token
 from webapps.jobs.forms import JobCreateForm
 from schemas.jobs import JobCreate
 from db.repository.jobs import create_new_job
+from typing import Optional
 
 
 templates = Jinja2Templates(directory="templates")
